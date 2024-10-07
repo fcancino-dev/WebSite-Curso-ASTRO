c5426b25334773cfa48b6cd856f5767e67c05164
Franco Cancino <franco.cancino@unapiquitos.edu.pe>
2024-10-07 00:11:34 -0500

    diseño terminado del temario de ProgramaCurso

--- src/components/capacitate/components/Temario.tsx
+++ src/components/capacitate/components/Temario.tsx
@@ -15,12 +15,10 @@ export default function Temario() {
     >
       <AccordionItem
         value="item-1"
-        className=" border border-[#C8C8C8] bg-white "
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4] "
       >
-        <AccordionTrigger className="border-b p-8 hover:no-underline">
-          <h2 className="text-xl font-medium hover:text-[#2729A4] text-[#515151]">
-            Fundamentos del desarrollo web
-          </h2>
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-xl font-medium text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          Fundamentos del desarrollo web
         </AccordionTrigger>
         <AccordionContent className="p-8">
           <p className="text-lg font-semibold pb-[10px]">
@@ -38,12 +36,10 @@ export default function Temario() {
       </AccordionItem>
       <AccordionItem
         value="item-2"
-        className=" border border-[#C8C8C8] bg-white "
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
       >
-        <AccordionTrigger className="border-b p-8 hover:no-underline">
-          <h2 className="text-[20px] hover:text-[#2729A4] text-[#515151]">
-            Fundamentos del desarrollo web
-          </h2>
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          Lenguaje de programacion: Python
         </AccordionTrigger>
         <AccordionContent className="p-8">
           <p className="text-lg font-medium pb-[10px]">
@@ -61,12 +57,115 @@ export default function Temario() {
       </AccordionItem>
       <AccordionItem
         value="item-3"
-        className=" border border-[#C8C8C8] bg-white "
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
       >
-        <AccordionTrigger className="border-b p-8 hover:no-underline">
-          <h2 className="text-[20px] hover:text-[#2729A4] text-[#515151]">
-            Fundamentos del desarrollo web
-          </h2>
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          Sistema de control de versiones: GIT
+        </AccordionTrigger>
+        <AccordionContent className="p-8">
+          <p className="text-lg font-medium pb-[10px]">
+            Introducción al desarrollo web: se presenta un resumen de las
+            principales tecnología para el desarrollo de aplicaciones web
+          </p>
+          <div className="text-base space-y-[10px]">
+            <li>
+              Principales tecnologías web: HTML, CSS, Frameworks de
+              Backend, Frameworks de Frontend, Herramientas en nube
+            </li>
+            <li>Funcionamiento de las aplicaciones web</li>
+          </div>
+        </AccordionContent>
+      </AccordionItem>{" "}
+      <AccordionItem
+        value="item-4"
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
+      >
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          HTML
+        </AccordionTrigger>
+        <AccordionContent className="p-8">
+          <p className="text-lg font-medium pb-[10px]">
+            Introducción al desarrollo web: se presenta un resumen de las
+            principales tecnología para el desarrollo de aplicaciones web
+          </p>
+          <div className="text-base space-y-[10px]">
+            <li>
+              Principales tecnologías web: HTML, CSS, Frameworks de
+              Backend, Frameworks de Frontend, Herramientas en nube
+            </li>
+            <li>Funcionamiento de las aplicaciones web</li>
+          </div>
+        </AccordionContent>
+      </AccordionItem>{" "}
+      <AccordionItem
+        value="item-5"
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
+      >
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          CSS
+        </AccordionTrigger>
+        <AccordionContent className="p-8">
+          <p className="text-lg font-medium pb-[10px]">
+            Introducción al desarrollo web: se presenta un resumen de las
+            principales tecnología para el desarrollo de aplicaciones web
+          </p>
+          <div className="text-base space-y-[10px]">
+            <li>
+              Principales tecnologías web: HTML, CSS, Frameworks de
+              Backend, Frameworks de Frontend, Herramientas en nube
+            </li>
+            <li>Funcionamiento de las aplicaciones web</li>
+          </div>
+        </AccordionContent>
+      </AccordionItem>{" "}
+      <AccordionItem
+        value="item-6"
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
+      >
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          Framework de desarrollo web: Django
+        </AccordionTrigger>
+        <AccordionContent className="p-8">
+          <p className="text-lg font-medium pb-[10px]">
+            Introducción al desarrollo web: se presenta un resumen de las
+            principales tecnología para el desarrollo de aplicaciones web
+          </p>
+          <div className="text-base space-y-[10px]">
+            <li>
+              Principales tecnologías web: HTML, CSS, Frameworks de
+              Backend, Frameworks de Frontend, Herramientas en nube
+            </li>
+            <li>Funcionamiento de las aplicaciones web</li>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem
+        value="item-7"
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
+      >
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          Bases de datos: MySQL
+        </AccordionTrigger>
+        <AccordionContent className="p-8">
+          <p className="text-lg font-medium pb-[10px]">
+            Introducción al desarrollo web: se presenta un resumen de las
+            principales tecnología para el desarrollo de aplicaciones web
+          </p>
+          <div className="text-base space-y-[10px]">
+            <li>
+              Principales tecnologías web: HTML, CSS, Frameworks de
+              Backend, Frameworks de Frontend, Herramientas en nube
+            </li>
+            <li>Funcionamiento de las aplicaciones web</li>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem
+        value="item-8"
+        className=" border-2 border-[#C8C8C8] bg-white data-[state=open]:border-[#2729A4]"
+      >
+        <AccordionTrigger className="border-b p-8 hover:no-underline text-[20px] text-[#515151] data-[state=open]:border-[#2729A4] data-[state=open]:text-[#2729A4]">
+          Javascript y React
         </AccordionTrigger>
         <AccordionContent className="p-8">
           <p className="text-lg font-medium pb-[10px]">
